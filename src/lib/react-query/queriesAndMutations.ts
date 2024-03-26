81e42665f8b3cce1860ad98aa7ddc146af15822a
Naarayana Kumar <naarayana.kumar@gmail.com>
2024-03-26 23:31:28 +0530

    removed infinite scroll for now

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -3,9 +3,8 @@ import {
     useQuery,
     useMutation,
     useQueryClient,
-    useInfiniteQuery,
   } from "@tanstack/react-query";
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, getUserById, getUserPosts, getUsers, likePost, savePost, searchPosts, signInAccount,  signOutAccount, updatePost, updateUser, } from "../appwrite/api";
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getPostById, getRecentPosts, getUserById, getUserPosts, getUsers, likePost, savePost, searchPosts, signInAccount,  signOutAccount, updatePost, updateUser, } from "../appwrite/api";
 import { QUERY_KEYS } from "./queryKeys";
 
   export const useCreateUserAccount = () => {
@@ -176,19 +175,7 @@ import { QUERY_KEYS } from "./queryKeys";
 // POST QUERIES
 // ============================================================
 
-export const useGetPosts = () => {
-  return useInfiniteQuery({
-    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      if (!lastPage || lastPage.documents.length === 0) return null;
 
-      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-
-      return lastId;
-    }
-  });
-};
 
 
 export const useSearchPosts = (searchTerm: string) => {
